45d320f5481528487b04454dfa61b80e763b1589
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-05-06 15:35:37 -0400

    Update Products.jsx
    This commit adds the basic layout for the products component at mobile size

--- e-commerce-clothing/src/Products.jsx
+++ e-commerce-clothing/src/Products.jsx
@@ -1,5 +1,40 @@
+import './global/grid.css'
+import filter from './assets/filter-icon.svg'
+
+
+
+
+
+
+
 export default function Products() {
     return (
-        <h1>This is the section for the products page.</h1>
+        <main>
+            <section className='grid-con'>
+
+                <div className='col-span-full'>
+                    <img src={filter} alt="Filter Icon" />
+                </div>
+
+                <section className='productCon col-span-full'>
+
+                    <div className='productCard'>
+                        <div>Image</div>
+                        <div>
+                            <p>Product Name</p>
+                            <p>Price</p>
+                        </div>
+                        <div className='ProductColours'>
+                            <div>Colors</div>
+                            <div>Colors</div>
+                            <div>Colors</div>
+                        </div>
+                        <button>Add To Cart</button>
+                    </div>
+                    
+                </section>
+
+            </section>
+        </main>        
     )
 }
