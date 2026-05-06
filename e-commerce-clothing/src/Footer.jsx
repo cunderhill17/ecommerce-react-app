9767a8a60542787d629a3a1294b12771330ce5ce
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-05-06 10:18:25 -0400

    Update Footer.jsx
    This commit updates the Footer.jsx file with the mobile layout for the footer

--- e-commerce-clothing/src/Footer.jsx
+++ e-commerce-clothing/src/Footer.jsx
@@ -1,5 +1,31 @@
+import './global/grid.css'
+
 export default function Footer() {
     return (
-        <h1>This is the Footer.</h1>
+        <footer className='grid-con'>
+            <h2 className="col-span-full">Name of Clothing Brand</h2>
+            
+            <div className='footerAddress col-span-full'>
+                <p>76 Willow Drive</p>
+                <p>London ON.</p>
+                <p>Canada, N6H1K2</p>
+            </div>
+            
+            <div className='col-span-full'>
+                <p>1 (000) 000-0000</p>
+            </div>
+
+            <div className='col-span-full'>
+                <p>&copy; 2026 Your Brand Name. All rights reserved. Designed with purpose.</p>
+            </div>
+
+            <div className='col-start-2 col-end-3'>
+                <p>Privacy</p>
+            </div>
+
+            <div className='col-start-3 col-end-4'>
+                <p>Terms</p>
+            </div>
+        </footer>
     )
 }
