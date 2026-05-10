61befd41ecee88b4d011928a22a54add3dcc4564
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-05-09 20:57:06 -0400

    Create Best Sellers Section
    Created bestSeller property in JSON data file and then exported ProductList component / imported to Homepage. Filtered product data based on bestSeller property and passed into ProductList to render to UI

--- e-commerce-clothing/src/Homepage.jsx
+++ e-commerce-clothing/src/Homepage.jsx
@@ -1,10 +1,24 @@
-import Newsletter from "./Newsletter"
+import { useOutletContext } from 'react-router-dom'
 
+import Newsletter from "./Newsletter"
+import { ProductList } from "./Products";
+import './global/grid.css'
+import './Homepage.css'
 
 export default function Homepage() {
+    const { cartItems, setCartItems, originalProducts } = useOutletContext();
+
+    const bestsellers = originalProducts.filter(p => p.bestSeller === true);
+
     return (
         <main>
 
+            <section className="grid-con">
+                <h2 className="col-span-full">Best Sellers</h2>
+                <div className="bestSellerCon col-span-full">
+                    <ProductList products={bestsellers} cartItems={cartItems} setCartItems={setCartItems}/>
+                </div>
+            </section>
 
             <Newsletter />
         </main>
